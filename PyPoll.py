5438287431426119a3e173c4359b5455713f1c2a
alelee93 <leevargasale@gmail.com>
2022-02-10 17:33:56 -0600

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -19,6 +19,13 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 #Open the election results and read the file
 with open(file_to_load) as election_data:
 
@@ -26,16 +33,43 @@ with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
     headers = next(file_reader)
-    print(headers)
+    # print(headers)
 
-    # for row in file_reader:
-    #     print(row)
+    for row in file_reader:
+        total_votes += 1
+        candidate_name = row[2]
+        
+        
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+# print(candidate_votes)
 
 #Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-    txt_file.write("Counties in the Election\n")
-    txt_file.write("--------------------------\n")
-    txt_file.write("Arapahoe\nDenver\nJefferson")
+# with open(file_to_save, "w") as txt_file:
+#     txt_file.write("Counties in the Election\n")
+#     txt_file.write("--------------------------\n")
+#     txt_file.write("Arapahoe\nDenver\nJefferson")
 
 
 # #use the open statement to openthe file as a text file.
